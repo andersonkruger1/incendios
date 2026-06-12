7343a1c71f7ae07daa3d65f5ff563d836b1c0e15
Anderson Kruger <116232416+andersonkruger1@users.noreply.github.com>
2026-06-12 15:00:55 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'iema-incendios-v1';
+const ASSETS = [
+  'registro_incendio.html',
+  'manifest.json',
+  'icone.png',
+  'https://cloudflare.com',
+  'https://jsdelivr.net'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
